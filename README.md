d9ab915aa83802f11fcea595be66438266b802ef
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-05-19 20:58:17 +0700

    delete file not used

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# klikdaily-test
+# test
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
-
-app.use(router);
-
-app.mount('#app');
+createApp(App).mount('#app')
