328c04c4e826f99adf7f153da4f079729001ef49
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-05-19 20:35:21 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import './index.css';
+</script>
+
+<template>
+    <header>
+        <div class="">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
